9cd3953cdcadccfd249ed749e7218890944375ad
matiasrj <matiasrj_89@hotmail.com>
2021-10-31 21:14:43 -0300

    Se agrega  interaccion mobile menu hamburguesa

---
+++ Proyecto/public/js/base.js
@@ -0,0 +1,15 @@
+// Futuros js
+import hamburguerMenu from "./hamburguer-menu.js";
+
+hamburguerMenu
+
+const d = document;
+
+d.addEventListener('DOMContentLoaded', (e)=>{
+   
+    hamburguerMenu(".fa-bars", ".panel-categorias");
+
+})
+
+
+
