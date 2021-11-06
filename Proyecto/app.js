d74df6b680faf111a97f1c0e566d7532c01ef2ad
matiasrj <matiasrj_89@hotmail.com>
2021-11-06 06:36:24 -0300

    Se agregan archivos styles_Home y Home.html. Se redirecciona url / a Home.html

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -10,7 +10,7 @@ app.use(express.static(publicPath))
 
 
 app.get('/', (req,res)=>{
-    res.sendFile( path.resolve(__dirname,'views/base.html'));
+    res.sendFile( path.resolve(__dirname,'views/Home.html'));
 })
 
 
