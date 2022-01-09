8dfc90b3460e3b036af0bb7523cd82d3ee680ceb
matiasrj <matiasrj_89@hotmail.com>
2022-01-09 08:53:43 -0300

    Formulario de Registro operativo.

--- Proyecto/controllers/usersController.js
+++ Proyecto/controllers/usersController.js
@@ -1,5 +1,8 @@
 const { request, response } = require("express");
 const path = require('path')
+const bcryptjs = require ('bcryptjs') 
+const fs = require ('fs')
+
 
 const userController = {
 
@@ -7,9 +10,76 @@ const userController = {
         res.render(path.resolve(__dirname ,'../views/users/Login_prov'));
     },
 
+    
+    
     register: (req=request, res= response)=> {
         res.render(path.resolve(__dirname ,'../views/users/Register_prov'));
+    },
+
+
+    processRegister : (req=request, res= response)=> {
+        const  {
+            nombreApe, 
+            nombreUsuario,
+            fechaNac,
+            Adress,
+            email,
+            password,
+            passwordConfirm,
+            imagenPerfil,
+        } = req.body;
+        
+        try {
+            
+        
+        // valido password
+        if (password !== passwordConfirm ){
+            return res.status(400).json({
+                error:{
+                    msg : 'Confirmacion de password incorrecta.'
+                }
+            })
+        }
+    
+        // Valido que no exista email registrado
+        let usuarios = require('../data/users.json');
+        let existUser = usuarios.find ( user => user.email === email);
+        if (existUser){
+            return res.status(400).json({
+                error:{
+                    msg : 'El email ya se encuentra registrado'
+                }
+            })
+        }
+
+        // encripto password
+        let passwordHash = bcryptjs.hashSync (password, 10);
+
+        const user = {
+            nombreApe, 
+            nombreUsuario,
+            fechaNac,
+            Adress,
+            email,
+            password : passwordHash,
+            avatar : req.file.filename
+        }
+
+
+        // Guardo nuevo usuario 
+        usuarios.push (user);
+        console.log(JSON.stringify(usuarios));
+        fs.writeFileSync('./data/users.json', JSON.stringify(usuarios));
+        res.render(path.resolve(__dirname ,'../views/users/Login_prov'));
+
+    } catch (error) {
+            console.log('No se pudo registrar el usuario')
     }
+
+    },
+
+
+
 }
 
 
--- Proyecto/routers/users.js
+++ Proyecto/routers/users.js
@@ -1,6 +1,7 @@
 const express= require('express')
 const { Router } = require("express");
 const userController = require("../controllers/usersController");
+const multer = require('multer');
 
 const path = require('path')
 const publicPath = path.resolve(__dirname, 'public');
@@ -8,9 +9,31 @@ const publicPath = path.resolve(__dirname, 'public');
 const router = Router();
 router.use(express.static(publicPath))
 
+//multer
+const storage = multer.diskStorage({
+    destination : (req,file,cb)=> {
+        let folder = path.join(__dirname, '../public/images/users');
+        cb(null, folder);
+    },
+    filename : (req,file,cb)=>{
+        let fileName = 'img_' + Date.now() + path.extname(file.originalname);
+        cb(null, fileName);
+    }
+});
+
+const upload = multer({storage});
+
+
+// Para recibir PUT / DELETE:
+const methodOverride = require('method-override');
+router.use(methodOverride('_method'));
+
+
 
 router.get('/login', userController.login);
+
 router.get('/register', userController.register);
+router.post('/register',upload.single('imagenPerfil'), userController.processRegister);
 
 
 module.exports = router;
