77dc03dae9591c6d9d328a6ad2da7c7e80d3a974
matiasrj <matias@inipop.com>
2022-03-15 21:52:45 -0300

    ADD Se agrega category como endpoint API

--- Proyecto/routers/api.js
+++ Proyecto/routers/api.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { categoryApiController } = require("../controllers/api/categoryApi");
 const { productsApiController } = require("../controllers/api/productsApi");
 const { usersApiController } = require("../controllers/api/usersApi");
 
@@ -14,5 +15,9 @@ router.get('/products/:id', productsApiController.detail);
 router.get('/users', usersApiController.list);
 router.get('/users/:id', usersApiController.detail);
 
+
+// API Categories
+router.get('/category', categoryApiController.list);
+router.get('/category/:id', categoryApiController.detail);
 
 module.exports = router;
